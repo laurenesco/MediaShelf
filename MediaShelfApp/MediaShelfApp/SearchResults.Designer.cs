1e767bc90d8ceacb2e3cf3032ef93c950c657ef2
eliseomrico <112983793+eliseomrico@users.noreply.github.com>
2023-02-21 03:36:26 -0600

    Changed the layout of Search Result Form v1

--- MediaShelfApp/MediaShelfApp/SearchResults.Designer.cs
+++ MediaShelfApp/MediaShelfApp/SearchResults.Designer.cs
@@ -29,7 +29,6 @@
         private void InitializeComponent()
         {
             this.searchBox1 = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
             this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
@@ -43,35 +42,26 @@
             this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
             this.listView1 = new System.Windows.Forms.ListView();
             this.label2 = new System.Windows.Forms.Label();
-            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // searchBox1
             // 
-            this.searchBox1.Location = new System.Drawing.Point(208, 55);
+            this.searchBox1.Location = new System.Drawing.Point(288, 54);
             this.searchBox1.Name = "searchBox1";
             this.searchBox1.PlaceholderText = "Search";
-            this.searchBox1.Size = new System.Drawing.Size(444, 23);
+            this.searchBox1.Size = new System.Drawing.Size(385, 23);
             this.searchBox1.TabIndex = 0;
             this.searchBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(672, 60);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(107, 15);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Can\'t find an item?";
-            // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(672, 78);
+            this.button1.Location = new System.Drawing.Point(29, 409);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(107, 71);
+            this.button1.Size = new System.Drawing.Size(145, 29);
             this.button1.TabIndex = 2;
-            this.button1.Text = "Make Manual Entry";
+            this.button1.Text = "Add Manual Entry";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // panel1
@@ -89,7 +79,7 @@
             this.panel1.Controls.Add(this.checkedListBox1);
             this.panel1.Location = new System.Drawing.Point(29, 55);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(145, 368);
+            this.panel1.Size = new System.Drawing.Size(145, 347);
             this.panel1.TabIndex = 3;
             // 
             // dateTimePicker2
@@ -189,7 +179,7 @@
             // 
             this.listView1.Location = new System.Drawing.Point(208, 97);
             this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(444, 326);
+            this.listView1.Size = new System.Drawing.Size(547, 305);
             this.listView1.TabIndex = 4;
             this.listView1.UseCompatibleStateImageBehavior = false;
             // 
@@ -197,32 +187,32 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label2.Location = new System.Drawing.Point(368, 9);
+            this.label2.Location = new System.Drawing.Point(416, 13);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(130, 28);
             this.label2.TabIndex = 5;
             this.label2.Text = "Media Search";
             // 
-            // button2
+            // label1
             // 
-            this.button2.Location = new System.Drawing.Point(601, 55);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(51, 23);
-            this.button2.TabIndex = 6;
-            this.button2.Text = "Search";
-            this.button2.UseVisualStyleBackColor = true;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(77, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 21);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Filters";
             // 
             // SearchResults
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.listView1);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.label1);
             this.Controls.Add(this.searchBox1);
             this.Name = "SearchResults";
             this.Text = "SearchResults";
@@ -236,7 +226,6 @@
         #endregion
 
         private TextBox searchBox1;
-        private Label label1;
         private Button button1;
         private Panel panel1;
         private Label label3;
@@ -250,6 +239,6 @@
         private Label label7;
         private ListView listView1;
         private Label label2;
-        private Button button2;
+        private Label label1;
     }
 }
