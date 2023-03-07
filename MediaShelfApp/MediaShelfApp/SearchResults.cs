b73c659083c7541947a57a485916470bf2ea4778
Jeremiah <jeremiah7407@gmail.com>
2023-03-06 20:11:25 -0600

    Added Search Functionality only for MovieDB

--- MediaShelfApp/MediaShelfApp/SearchResults.cs
+++ MediaShelfApp/MediaShelfApp/SearchResults.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Data.SqlClient;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -51,15 +52,59 @@ namespace MediaShelfApp
             this.searchText = text;
         }
 
+        // What API parses for
+        public class MovieInfo
+        {
+            public string original_title { get; set; }
+            public string overview { get; set; }
+        }
+
+        // Separates the results in JSON and puts it into a list format
+        public class MovieResult
+        {
+            public List<MovieInfo> results { get; set; }
+        }
+
         //////////////////////
         //  Priate methods  //
         //////////////////////
-        
+
         // Back button functionality - reopens the calling form and closes this form
         private void btnNavBack_Click(object sender, EventArgs e)
         {
             caller.Show();
             this.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            using (var client = new HttpClient())
+            {
+                string query = searchBox1.Text.ToString();
+
+                // API Key is in code, need to clean up and add as private variables at later date / clean up URI as well
+                var endpoint = new Uri("https://api.themoviedb.org/3/search/movie?api_key=d076abf5fac2dab8dfeaca89a50f37a2&query=" + query);
+                var result = client.GetAsync(endpoint).Result;
+                var json = result.Content.ReadAsStringAsync().Result;
+                var movieList = JsonConvert.DeserializeObject<MovieResult>(json);
+
+                richTextBox1.Clear();
+
+                //Displays Top 10 results, breaks loop if none
+                for (int i = 0; i < 10; i++)
+                {
+                    try
+                    {
+                        richTextBox1.AppendText("Title: " + movieList.results[i].original_title.ToString() + "\n"
+                            + "Overview: " + movieList.results[i].overview.ToString() + "\n\n");
+                    }
+                    catch
+                    {
+                        break;
+                    }
+                }
+
+            }
         }
     }
 }
