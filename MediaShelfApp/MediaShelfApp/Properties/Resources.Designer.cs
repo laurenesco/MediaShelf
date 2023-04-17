138d5e5aa7ee2a573c25641843e1e07b7c22ab1d
IL-faith <112983859+IL-faith@users.noreply.github.com>
2023-04-16 22:20:24 -0700

    Added background image to project resources.
    Forgot to add this in earlier commits.

--- MediaShelfApp/MediaShelfApp/Properties/Resources.Designer.cs
+++ MediaShelfApp/MediaShelfApp/Properties/Resources.Designer.cs
@@ -63,6 +63,16 @@ namespace MediaShelfApp.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap BACKGROUND_WHITE {
+            get {
+                object obj = ResourceManager.GetObject("BACKGROUND WHITE", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap search {
             get {
                 object obj = ResourceManager.GetObject("search", resourceCulture);
